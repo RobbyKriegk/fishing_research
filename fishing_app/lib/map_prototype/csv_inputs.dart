074058d83389daa25e65d280c43b0264eca3b100
Robby <robbykriegk@live.de>
2023-11-10 15:49:36 +0100

    function for adding marker per csv-data added

---
+++ fishing_app/lib/map_prototype/csv_inputs.dart
@@ -0,0 +1,9 @@
+import "dart:convert";
+import "dart:io";
+import "package:csv/csv.dart";
+
+// final input = File('assets/logger_5_deployment_152.csv').openRead();
+// final fields =
+//     input.transform(utf8.decoder).transform(CsvToListConverter()).toList();
+
+// csvListCreator() {}
--- fishing_app/lib/map_prototype/map_prototype.dart
+++ fishing_app/lib/map_prototype/map_prototype.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv.dart';
 import 'package:fishing_app/map_prototype/zoom_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -15,12 +19,71 @@ class _MapPrototypeState extends State<MapPrototype> {
   // MapCamera mapCamera = MapCamera(
   //   zoom: 8,
   // );
+  List<Map<String, dynamic>> mapPoints = [];
+
+  Future<List<Map<String, dynamic>>> csvListCreator(
+      String csv, List<Map<String, dynamic>> localMap) async {
+    List pointsList = [];
+
+    final input = File(csv).openRead();
+    final fields = await input
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter())
+        .toList();
+    for (int i = 1; i < fields.length; i++) {
+      String pointString = fields[i][2];
+      pointString = pointString.replaceAll('POINT', '');
+      pointString = pointString.replaceAll('(', '');
+      pointString = pointString.replaceAll(')', '');
+      pointsList = pointString.split(' ');
+      localMap.add({
+        'lat': double.parse(pointsList[1]),
+        'lng': double.parse(pointsList[0]),
+      });
+    }
+    //print(localMap);
+    return localMap;
+  }
+
+  createMarkerRoad(List<Map<String, dynamic>> localMap) {
+    List<Marker> markerList = [];
+    for (int i = 0; i < localMap.length; i++) {
+      // if (localMap.isEmpty) {
+      //   localMap.add({
+      //     'lat': 53.998308,
+      //     'lng': 10.974112,
+      //   });
+      // }
+      setState(() {
+        markerList.add(Marker(
+          point: LatLng(localMap[i]['lat'], localMap[i]['lng']),
+          width: 60,
+          height: 60,
+          child: Image.asset('assets/images/freudiger_fisch.png'),
+        ));
+      });
+    }
+    return markerList;
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   setState(() {
+  //     csvListCreator(
+  //         '/Users/robbykriegk/Desktop/development/05-app-fishen-a/fishing_app/assets/logger_5_deployment_152.csv',
+  //         mapPoints);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    csvListCreator(
+        '/Users/robbykriegk/Desktop/development/05-app-fishen-a/fishing_app/assets/logger_5_deployment_152.csv',
+        mapPoints);
     return FlutterMap(
       mapController: mapController,
-      options: MapOptions(
+      options: const MapOptions(
         initialCenter: LatLng(54, 12),
         initialZoom: 10,
       ),
@@ -31,14 +94,7 @@ class _MapPrototypeState extends State<MapPrototype> {
           userAgentPackageName: 'unknown',
         ),
         MarkerLayer(
-          markers: [
-            Marker(
-              point: LatLng(53.998308, 10.974112),
-              width: 80,
-              height: 80,
-              child: Image.asset('assets/images/freudiger_fisch.png'),
-            ),
-          ],
+          markers: createMarkerRoad(mapPoints),
         ),
         Column(
           children: [
