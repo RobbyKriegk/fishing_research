6351c953154838f9902ffbbff8015a7718f38520
RobbyKriegk <robbykriegk@live.de>
2023-12-21 11:43:52 +0100

    commit for serverchanges

--- fishing_app_backend/hello.py
+++ fishing_app_backend/hello.py
@@ -1,9 +1,12 @@
 import subprocess
 from flask import Flask
+from main2 import * ;
+from main import * ;
 
-app = Flask(__name__)
+app = Flask(__name__, )
+
 
 @app.route("/start_main")
 def start_main():
-    subprocess.call(["python", "main.py"])
-    return "Started main.py"
+    #subprocess.call(["python", "main.py"])
+    return returnCSV()
