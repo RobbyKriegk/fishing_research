2e5487bfd818f800b27480cfdd2dee3cc1938f27
Robby <robbykriegk@live.de>
2023-12-22 10:55:37 +0100

    initial commit + commit for hosting server online via vercel

---
+++ fishing_app_backend/app.py
@@ -0,0 +1,12 @@
+import subprocess
+from flask import Flask
+from main2 import * ;
+from main import * ;
+
+app = Flask(__name__)
+
+
+@app.route("/start_main")
+def start_main():
+    #subprocess.call(["python", "main.py"])
+    return returnCSV()
